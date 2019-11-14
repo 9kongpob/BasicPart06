6a09d72f9fd7ef333d5bc4e587d7ed0ef589df32
IT4 <IT4@siwt.co.th>
2019-11-14 14:33:35 +0700

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace BasicPart06_Array
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string nameA = "kong";
+            string nameB = "Jojo";
+            string nameC = "Num";
+            string[] name = {nameA,nameB,nameC };
+
+            Console.WriteLine("Check nameA : " + nameA);
+            Console.WriteLine("Check nameB : " + nameB);
+            Console.WriteLine("Check nameC : " + nameC);
+            Console.WriteLine("Check name.Length : " + name.Length);
+
+            int count = 0;
+            foreach (string i in name)
+            {
+                Console.WriteLine("Check Array[" + count + "] : " + i);
+                count++;
+            }
+            /*for (int i = 0; i < name.Length; i++)
+            {
+                Console.WriteLine("Check Array["+i+"] : " + name[i]);
+            }*/
+
+            Console.ReadKey();
+        }
+    }
+}
